a3abad72ec2938bf4cb46b6144d7667a5fea8dd6
Daniel Toader & Tom Mertz <student@epicodus.com>
2015-04-07 09:59:22 -0700

    add logic for words to ignore

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,20 @@
 function getTitleCase(input) {
-
+  var ignore = ["a", "an", "the", "and", "but", "or", "nor", "at", "by", "for", "from", "in", "into", "of", "off", "on", "onto", "out", "up", "with", "to", "as"];
   var words = input.split(" ");
 
   var words_uppercase = [];
-  words.forEach(function(word) {
-    words_uppercase.push(word[0].toUpperCase() + word.substring(1));
+  words.forEach(function(word, index) {
+    console.log("Index is " + index);
+    if (index === 0 || index === words.length - 1) {
+      words_uppercase.push(word[0].toUpperCase() + word.substring(1));
+    } else {
+      if ($.inArray(word, ignore) !== -1) {
+        words_uppercase.push(word);
+      } else {
+        words_uppercase.push(word[0].toUpperCase() + word.substring(1));
+      }
+    }
+
   });
 
   return words_uppercase.join(" ");
--- spec/specs.js
+++ spec/specs.js
@@ -1,6 +1,6 @@
 describe('getTitleCase', function() {
   it("capitalizes one letter word", function() {
-    expect(getTitleCase("a")).to.equal("A");
+    expect(getTitleCase("z")).to.equal("Z");
   });
 
   it("capitalizes multiple letter word", function() {
@@ -11,8 +11,16 @@ describe('getTitleCase', function() {
     expect(getTitleCase("i have ten pet robots")).to.equal("I Have Ten Pet Robots");
   });
 
-  it("does not capitalize articles if not first or last words", function() {
+  it("does not capitalize articles", function() {
     expect(getTitleCase("i have a pet robot")).to.equal("I Have a Pet Robot");
+  });
+
+  it("capitalizes articles that are the first and last word", function() {
+    expect(getTitleCase("a night to dream an")).to.equal("A Night to Dream An");
+  });
+
+  it("capitalizes all words in a string but articles, conjuctions, pepositions and infinitives", function() {
+    expect(getTitleCase("a cat onto the rug is by the fire")).to.equal("A Cat onto the Rug Is by the Fire");
   });
 
 });
